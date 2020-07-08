d776391935ac8faf02014b3184ec5e4d0f79f77c
Zafer Dogan <zafer.dogan.ext@nokia.com>
2020-07-08 20:32:16 +0200

    add time reports

---
+++ plugins/redmine_queries_for_time_report/README.md
@@ -0,0 +1,108 @@
+redmine_queries_for_time_report
+===============================
+
+Redmine plugin that adds Queries to **Time Report** like **Time Entry Queries**,
+
+&nbsp;&nbsp;It allows to add such saved queries to **My Page**
+
+&nbsp;&nbsp;Be careful, because of the nature of Time Report, records count will **not be limited** to 10, unlike other My Page Blocks.
+
+It also allows to add Time Entry queries to **My Page**
+
+# How it works
+
+## What it does
+
+* Adds : specific configurations in the **Report tab** of the Timelog page
+
+  To be able to configure and save Time Report Queries
+
+  Enables features provided by **redmine_extended_queries** plugin
+
+  Because these two plugins override the same views
+
+* Tested with **Redmine V4.0.3**
+
+## How it is implemented
+
+- 🔑 Rewrites the **report** **TimelogController** method
+
+- 🔑 Rewrites the following **views**
+
+  - To add icons to the query block title
+
+    - /my/blocks/_calendar.html.erb
+    - /my/blocks/_documents.html.erb
+    - /my/blocks/_issues.erb
+    - /my/blocks/_news.html.erb
+    - /my/blocks/_timelog.html.erb
+
+  - To add the project in the query block title
+
+    - /my/blocks/_issue_query_selection.html.erb
+
+- Adds / Rewrites the following **My Page** block **views**
+
+    - /my/blocks/partials/_timelogs.html.erb
+    - /my/blocks/partials/_timereport.html.erb
+
+- Adds **My Page** block **views** helper partials
+
+    - /my/blocks/partials/_timelog_query_selection.html.erb
+    - /my/blocks/partials/_timereport_query_selection.html.erb
+
+- Adds new **TimeReportQuery** model
+
+- 🔑 Extends **My Helper** by adding the methods :
+
+  - **render_timelogquery_block**
+
+  - **render_timereportquery_block**
+
+- 🔑 Extends **Timelog Helper** by adding the methods :
+
+  - **prepare_report_object**
+
+  - **time_entry_scope**
+
+- 🔑 Extends **Redmine::MyPage** to :
+
+  - Add new My Page blocks in the **CORE_BLOCKS** hash
+  - Override **additional_blocks** class method to manage partials overriden in plugins
+
+- No migration ! uses Single table inheritance Redmine model.
+
+- No new route !
+
+# TODOs
+
+* Add Tests
+* Fix TODOS
+* Complete the overriden / added files
+
+# To test it
+
+```console
+# From plugin root, redmine_test mysql database must exist
+scripts/test_it.sh
+```
+
+# Changelog
+
+* **V1.3.8**  Enables **Enumeration** (Key/Value) Custom Fields in Time Report **criteria**
+* **V1.0.7**  Enables **String Format** Custom Fields in Time Report **criteria**
+* **V1.0.6**  Compatibility with RM V4.1+ #31433 of RM.org for RM > 4.1
+* **V1.0.5**  **criteria selection** at the beginning
+* **V1.0.4**  **available_criteria_options** : sorted criteria + prefix by glyphs icons
+
+  Time Report criteria list : criteria are now sorted by type (Tree, Custom Fields, Date)
+
+* **V1.0.3**  Redmine V3.4.6 minimum, + UserCustomFields in TimeReport Criteria
+* **V1.0.2**  Enabled optional features provided in redmine_extended_queries plugin
+because these two plugins override the same views :
+**app/views/queries/_query_form.html.erb** and **app/views/queries/_form.html.erb**
+* **V1.0**    Initial version
+
+Enjoy !
+
+<kbd>![alt text](https://compteur-visites.ennder.fr/sites/37/token/githubtrq/image "Logo") <!-- .element height="10%" width="10%" --></kbd>
---
+++ plugins/redmine_queries_for_time_report/lib/models/smile_models_query.rb
@@ -0,0 +1,120 @@
+# Smile - add methods to the Query model
+#
+# 1/ module Query4TimeReport
+# * #784714 V4.0.0 : My page Time Entry queries
+
+#require 'active_support/concern' #Rails 3
+
+module Smile
+  module Models
+    module QueryOverride
+      #********************
+      # 1/ Query4TimeReport
+      module Query4TimeReport
+        # extend ActiveSupport::Concern
+
+        def self.prepended(base)
+          #####################
+          # 1/ Instance methods
+          time_report_queries_instance_methods = [
+            :name_and_project, # 1/ new method          RM 4.0.0 OK
+          ]
+
+          trace_prefix = "#{' ' * (base.name.length + 27)}  --->  "
+          last_postfix = '< (SM::MO::QueryOverride::Query4TimeReport)'
+
+
+          smile_instance_methods = base.public_instance_methods.select{|m|
+              base.instance_method(m).owner == self
+            }
+          smile_instance_methods += base.protected_instance_methods.select{|m|
+              base.instance_method(m).owner == self
+            }
+
+          missing_instance_methods = time_report_queries_instance_methods.select{|m|
+            !smile_instance_methods.include?(m)
+          }
+
+          if missing_instance_methods.any?
+            trace_first_prefix = "#{base.name} MISS            instance_methods  "
+          else
+            trace_first_prefix = "#{base.name}                 instance_methods  "
+          end
+
+          SmileTools::trace_by_line(
+            (
+              missing_instance_methods.any? ?
+              missing_instance_methods :
+              smile_instance_methods
+            ),
+            trace_first_prefix,
+            trace_prefix,
+            last_postfix,
+            :redmine_queries_for_time_report
+          )
+
+          if missing_instance_methods.any?
+            raise trace_first_prefix + missing_instance_methods.join(', ') + '  ' + last_postfix
+          end
+
+
+          ##################
+          # 2/ Class methods
+          time_report_queries_class_methods = [
+            :max_criteria,       # 1/  new method  RM 4.0.3 OK
+          ]
+
+          base.singleton_class.prepend ClassMethods
+
+          last_postfix = '< (SM::MO::QueryOverride::Query4TimeReport::CMeths)'
+
+          smile_class_methods = base.methods.select{|m|
+              base.method(m).owner == ClassMethods
+            }
+
+          missing_class_methods = time_report_queries_class_methods.select{|m|
+            !smile_class_methods.include?(m)
+          }
+
+          if missing_class_methods.any?
+            trace_first_prefix = "#{base.name} MISS                     methods  "
+          else
+            trace_first_prefix = "#{base.name}                          methods  "
+          end
+
+          SmileTools::trace_by_line(
+            (
+              missing_class_methods.any? ?
+              missing_class_methods :
+              smile_class_methods
+            ),
+            trace_first_prefix,
+            trace_prefix,
+            last_postfix,
+            :redmine_queries_for_time_report
+          )
+
+          if missing_class_methods.any?
+            raise trace_first_prefix + missing_class_methods.join(', ') + '  ' + last_postfix
+          end
+        end # def self.prepended
+
+        # 1/ new method          RM 4.0.3 OK
+        # Smile specicfic #784714 V4.0.0 : My page Time Entry queries
+        def name_and_project
+          return name unless project
+
+          "#{::ERB::Util.h name}&nbsp;&nbsp;|&nbsp;&nbsp;#{::ERB::Util.h project.name}".html_safe
+        end
+  
+        module ClassMethods
+          # 1/ new method          RM 4.0.3 OK
+          # Smile specicfic #784714 V4.0.0 : My page Time Entry queries
+          def max_criteria
+            3
+          end
+        end
+      end # module Query4TimeReport
+    end # module QueryOverride
+  end # module Models
+end # module Smile
