890ac4950f9c3cef08e4cddbd6c83d38d53158cd
Zafer Dogan <zafer.dogan.ext@nokia.com>
2020-07-09 07:31:46 +0200

    added mail to prod conf, in ruby settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,4 +24,14 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+  
+  ActionMailer::Base.smtp_settings = {
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :authentication => :plain,
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => ENV['MAILGUN_DOMAIN']
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
