8f311760c53fe46d12c0f4a640223d49fafb2006
Zafer Dogan <zafer.dogan.ext@nokia.com>
2020-07-09 09:01:58 +0200

    fixed wrong smtp for gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
      :user_name => ENV['GMAIL_USERNAME'],
      :password => ENV['GMAIL_PASSWORD'],
      :domain => ENV['HEROKU_APP_DOMAIN'],
-     :address => 'smtp.gmail.net',
+     :address => 'smtp.gmail.com',
      :port => 587,
      :authentication => :plain,
      :enable_starttls_auto => true
